d76260067ab113b065516d708b9634f4392d0755
Renia in RainyLand <rainyxeon@gmail.com>
2026-02-28 02:37:35 +0000

    add: hooks and new package name

---
+++ app/src/main/java/com/dpr/rainyxeon/system_service_spoofer/TabletLandscapeModule.java
@@ -0,0 +1,46 @@
+package com.tablet.splitqs;
+
+import android.content.res.Configuration;
+import android.content.res.Resources;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class TabletLandscapeModule implements IXposedHookLoadPackage {
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
+        if (!lpparam.packageName.equals("android")) return;
+
+        try {
+            hookListService();
+        } catch (Throwable t) {
+            XposedBridge.log(t);
+        }
+    }
+
+    private void hookListService() {
+        XposedHelpers.findAndHookMethod(
+            "android.os.ServiceManager",
+            null,
+            "listServices",
+            new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) {
+                    String[] services = (String[]) param.getResult();
+                    if (services == null) return;
+                    List<String> filtered = new ArrayList<>();
+                    for (String s : services) {
+                        if (!s.contains("lineage")) {
+                            filtered.add(s);
+                        }
+                    }
+                    param.setResult(filtered.toArray(new String[0]));
+                }
+            }
+        );
+    }
+}
